0a54b35b2188cd74d68ea10e9e3c3876494a9b96
Mark <mark@gandolfo.com.au>
2024-07-09 22:01:35 +1000

    vga text mode implemented

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // VGA Buffer is located at address 0xb8000 and that each
-    // character cell consists of an ASCII char and a color byte
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0F;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }
