bacaf4a773b3abb16522f038ee0d7fa69573ed69
Mark <mark@gandolfo.com.au>
2024-07-11 15:04:35 +1000

    kernel handles cpu exceptions

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,15 @@ use mark_os::println;
 pub extern "C" fn _start() -> ! {
     println!("hello world{}", "!");
 
+    mark_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
